960005a71326ea940bb8ea9b613b755189d5fa25
Nick Jardine <njardine@freshlime.com>
2024-12-28 16:01:04 -0700

    Add a view engine and basic styles

--- app.js
+++ app.js
@@ -3,20 +3,37 @@ const express = require("express");
 //express app
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Hollow Knight Pantheon 5 Walkthrough",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How To Beat Ludwig The Accursed",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "The Bazaar | Vanessa One Weapon Guide",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
