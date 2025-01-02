f0fae593aa72d8c4608a6a172d584e51644090b5
Nick Jardine <nickjar86@gmail.com>
2025-01-01 17:02:01 -0700

    Add endpoints for getting/deleting/adding blogs

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.redirect("/blogs");
@@ -29,6 +30,8 @@ app.get("/about", (req, res) => {
 });
 
 //blog routes
+
+//get all blogs
 app.get("/blogs", (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
@@ -36,6 +39,45 @@ app.get("/blogs", (req, res) => {
       res.render("index", { title: "All Blogs", blogs: result });
     })
     .catch((err) => {});
+});
+
+//create a new blog
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//get a specific blog
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((results) => {
+      res.render("details", { blog: results, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//delete a blog
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
