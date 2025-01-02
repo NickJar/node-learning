569fb5c48d760cfa381f0c9cce30dc48a23af2a5
Nick Jardine <nickjar86@gmail.com>
2025-01-01 19:45:39 -0700

    Add trash can icon and handle 404's for blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -18,7 +18,7 @@ const blog_details = (req, res) => {
       res.render("blogs/details", { blog: results, title: "Blog Details" });
     })
     .catch((err) => {
-      console.log(err);
+      res.status(404).render("404", { title: "Page not found" });
     });
 };
 
